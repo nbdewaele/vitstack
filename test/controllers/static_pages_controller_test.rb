a3f49dae6e3008370728ba2187e2e168e40c3fae
Nate DeWaele <nbdewaele@gmail.com>
2017-12-03 08:51:13 -0500

    views edited and template view set in application.html.erb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-		assert_select "title", "Home | BRBBaby Home Page"
+		assert_select "title", "Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-		assert_select "title", "Help | BRBBaby Help"
+		assert_select "title", "Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-		assert_select "title", "About | About BRBBaby"
+		assert_select "title", "About"
   end
 
 end
