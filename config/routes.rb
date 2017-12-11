c8c6e96d4e64dd8f62e5dd70369c1c8217c3c9ab
Nate DeWaele <nbdewaele@gmail.com>
2017-12-11 09:40:02 -0500

    login added and links added to controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
 	get 'static_pages/contact'
 
+	get 'static_pages/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
