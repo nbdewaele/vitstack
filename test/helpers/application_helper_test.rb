fd0f41fc504cda478564844604efb6a16f35afa8
Nate DeWaele <nbdewaele@gmail.com>
2017-12-22 23:18:56 -0500

    user sign up and security features added

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,7 +4,7 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
 
 		# This commented test may be needed later
-    # assert_equal full_title,         FILL_IN
+    # assert_equal full_title,         FILL_IN - find what to fill in and uncomment
 
 
     assert_equal full_title("Help"), "Help | BRBBaby"
