7da63a0c70065b50eebb4aa178eedd2b7e8f08f4
Nate DeWaele <nbdewaele@gmail.com>
2017-12-31 14:50:30 -0500

    secure login and logout features added but not finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+			remember user
 			log_in user
       redirect_to user
     else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
 	def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
