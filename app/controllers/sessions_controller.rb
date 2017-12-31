565af25ef597d85f6c0e932e862e8a1e5a041d02
Nate DeWaele <nbdewaele@gmail.com>
2017-12-31 18:30:12 -0500

    sessions features added, more needed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
   end
 
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-			remember user
-			log_in user
-      redirect_to user
+		?user = User.find_by(email: params[:session][:email].downcase)
+    if ?user && ?user.authenticate(params[:session][:password])
+      log_in ?user
+      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+      redirect_to ?user
     else
       # Create an error message.
 			flash.now[:danger] = 'Invalid email/password combination'
