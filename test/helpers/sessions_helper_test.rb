0821b56e99cb5021c52618a487676229b324a99b
Nate DeWaele <nbdewaele@gmail.com>
2018-01-01 12:44:49 -0500

    errors and failures need to be fixed for user and session

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -7,19 +7,6 @@ class SessionsHelperTest < ActionView::TestCase
     remember(@user)
   end
 
-	# Returns the user corresponding to the remember token cookie.
- def current_user
-	 if (user_id = session[:user_id])
-		 @current_user ||= User.find_by(id: user_id)
-	 elsif (user_id = cookies.signed[:user_id])
-		 user = User.find_by(id: user_id)
-		 if user && user.authenticated?(cookies[:remember_token])
-			 log_in user
-			 @current_user = user
-		 end
-	 end
- end
-
   test "current_user returns right user when session is nil" do
     assert_equal @user, current_user
     assert is_logged_in?
