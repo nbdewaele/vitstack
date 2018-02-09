8ba7a1552972edd226f18ba35a5a315741f823cd
Nate DeWaele <nbdewaele@gmail.com>
2018-02-09 12:47:46 -0500

    experimental solution

--- app/controllers/product_formulas_controller.rb
+++ app/controllers/product_formulas_controller.rb
@@ -3,14 +3,7 @@ class ProductFormulasController < ApplicationController
   end
 
   def create
-		@formula_profile = FormulaProfile.new(formula_profile_params)
-    if @formula_profile.save
-      flash[:info] = "Awesome-sauce, let's add ingredients to your profile"
-			# do I redirect to ingredients or formula profile to add ingredients
-      # redirect_to root_url
-    else
-      render 'new'
-    end
+
   end
 
   def update
@@ -26,12 +19,12 @@ class ProductFormulasController < ApplicationController
   end
 
 	def new
-    @formula_profile = FormulaProfile.new
+
   end
 
-	private
-
-    def formula_profile_params
-      params.require(:formula_profile).permit(:formula_name)
-    end
+	# private
+  #
+  #   def formula_profile_params
+  #     params.require(:formula_profile).permit(:formula_name)
+  #   end
 end
