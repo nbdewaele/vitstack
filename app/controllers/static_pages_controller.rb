ceb4fa6dcbe0c36f97ff875033d5775a8be22b93
Nate DeWaele <nbdewaele@gmail.com>
2018-02-14 08:42:19 -0500

    form removed till it can be fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,23 +14,5 @@ class StaticPagesController < ApplicationController
 	# def login
   # end
 	def new
-		@formula_profile = FormulaProfile.new
 	end
-
-	def create
-		@formula_profile = FormulaProfile.new(formula_profile_params)
-    if @formula_profile.save
-      flash[:info] = "Awesome-sauce, let's add ingredients to your profile"
-			# do I redirect to ingredients or formula profile to add ingredients
-      # redirect_to root_url
-    else
-      render 'new'
-    end
-	end
-
-	private
-
-    def formula_profile_params
-      params.require(:formula_profile).permit(:formula_name)
-    end
 end
